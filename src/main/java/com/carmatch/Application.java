013317d867b73aa725b48b946059296da6ffabe4
Zoha Qureshi <zoha.qureshi2310@gmail.com>
2026-07-19 19:02:42 +0530

    make OTP email sending async to prevent registration timeout

--- src/main/java/com/carmatch/Application.java
+++ src/main/java/com/carmatch/Application.java
@@ -3,13 +3,12 @@ package com.carmatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
-
 	public static void main(String[] args) {
-
-		ApplicationContext context=SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
 	}
-
 }
--- src/main/java/com/carmatch/service/EmailService.java
+++ src/main/java/com/carmatch/service/EmailService.java
@@ -3,6 +3,7 @@ package com.carmatch.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendOtpEmail(String toEmail, String otp) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
